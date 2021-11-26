360e2289e0f66fabdb98d47070282bdb05d4d857
fazemodz <45793524+fazemodz@users.noreply.github.com>
2021-11-26 08:47:36 +0000

    updated folder struct

---
+++ frontend/src/modules/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import VideoBackground from './headercomponents/VideoBackground'
+import HeaderText from './headercomponents/headertext'
+export default function Header() {
+    return (
+        <div>
+            <VideoBackground />
+            <HeaderText />
+        </div>
+    )
+}
---
+++ frontend/src/modules/navlinks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function navlinks() {
+    return (
+        <div className="NavBar">
+            <div className="PageLink">
+                <a  href="#"> Test</a>
+            </div>
+        </div>
+    )
+}
